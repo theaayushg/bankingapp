89c15d7102081728c7f16601aabb59efb940b938
Aayush Gupta <AayushGuptap15@gmail.com>
2025-02-02 18:39:43 -0500

    First commit, added basic structure for program

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,28 @@
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main>
+        SIDEBAR
+        {children}
+    </main>
+  );
+}
+
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   return (
+//     <html lang="en">
+//       <body
+//         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+//       >
+//         {children}
+//       </body>
+//     </html>
+//   );
+// }
---
+++ app/layout.tsx
@@ -0,0 +1,56 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, Inter, IBM_Plex_Serif } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const ibmPlexSerif = IBM_Plex_Serif({
+  subsets: ["latin"],
+  weight: ['400','700'],
+  variable: "--font-ibm-plex-serif"
+});
+
+export const metadata: Metadata = {
+  title: "Horizon Bank",
+  description: "Horizon Bank is a modern banking platform for everyone.",
+  icons: {
+    icon: '/icon/logo.svg'
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} ${ibmPlexSerif.variable}`}>{children}</body>
+    </html>
+  );
+}
+
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   return (
+//     <html lang="en">
+//       <body
+//         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+//       >
+//         {children}
+//       </body>
+//     </html>
+//   );
+// }
