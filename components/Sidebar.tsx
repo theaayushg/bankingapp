212750a3256db476e2b312f95edc6485211ec444
Aayush Gupta <AayushGuptap15@gmail.com>
2025-02-04 21:48:05 -0500

    Implement draft sidebar buttons and routes

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,12 @@
+'use client'
 import Link from 'next/link'
 import Image from 'next/image'
+import { sidebarLinks } from '@/constants'
+import { cn } from '@/lib/utils'
+import { usePathname } from 'next/navigation'
 
 const Sidebar = ({user}: SiderbarProps) => {
+  const pathname = usePathname();
   return (
     <section className="sidebar">
         <nav className="flex flex-col gap-4">
@@ -16,6 +21,24 @@ const Sidebar = ({user}: SiderbarProps) => {
                 />
                 <h1 className="sidebar-logo">Horizon</h1>
             </Link>
+
+            {sidebarLinks.map((item) => {
+              const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`)
+
+              return (
+                <Link href={item.route} key={item.label} className={cn('sidebar-link',{'bg-bank-gradient': isActive})}>
+                  <div className="relative size-6">
+                    <Image
+                      src={item.imgURL}
+                      alt={item.label}
+                      fill
+                    />
+                  </div>
+                </Link>
+
+              )
+              }
+            )}
         </nav>
     </section>
   )
