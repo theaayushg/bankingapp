209f6e3606663ac79a00c0bda7c7c32876ecfa2e
Aayush Gupta <AayushGuptap15@gmail.com>
2025-02-04 23:00:55 -0500

    Implement mobile basic setup for layout

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -10,7 +10,7 @@ const Sidebar = ({user}: SiderbarProps) => {
   return (
     <section className="sidebar">
         <nav className="flex flex-col gap-4">
-            <Link href="/" className="mb-12 cursor-pointer items-center gap-2">
+            <Link href="/" className="mb-12 cursor-pointer flex items-center gap-2">
                 <Image
                     src="icons/logo.svg"
                     width={34}
@@ -32,14 +32,20 @@ const Sidebar = ({user}: SiderbarProps) => {
                       src={item.imgURL}
                       alt={item.label}
                       fill
+                      className={cn({'brightness-[3] invert-0':isActive})}
                     />
                   </div>
+                  <p className={cn('sidebar-label',{'!text-white':isActive})}>
+                    {item.label}
+                  </p>
                 </Link>
 
               )
               }
             )}
+            USER
         </nav>
+        FOOTER
     </section>
   )
 }
