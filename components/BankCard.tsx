5a4d6245a60dfa14537dad84f6c813c71ec1e52b
Aayush Gupta <AayushGuptap15@gmail.com>
2025-02-10 09:12:49 -0500

    Finished sidebars with fixed data along with basic routing

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -23,11 +23,11 @@ const BankCard = ({account, userName, showBalance = true}: CreditCardProps) => {
                             {userName}
                         </h1>
                         <h2 className="text-12 font-semibold text-white">
-                            ** / **
+                            ●● / ●●
                         </h2>
                     </div>
-                    <p className="text-14 font-semibold tracing-[1.1px] text-white">
-                        **** **** **** **** <span className="text-16">${account.mask}</span>
+                    <p className="text-14 font-semibold tracking-[1.1px] text-white">
+                        ●●●● ●●●● ●●●● <span className="text-16">1234</span>
                     </p>
                 </article>
             </div>
@@ -44,9 +44,18 @@ const BankCard = ({account, userName, showBalance = true}: CreditCardProps) => {
                     width={45}
                     height={32}
                     alt="mastercard"
+                    className="ml-4"
                 />
             </div>
+            <Image 
+                src="/icons/lines.png"
+                width={316}
+                height={190}
+                alt="lines"
+                className="absolute top-0 left-0"
+            />
         </Link>
+        {/* COPY */}
     
     </div>
   )
