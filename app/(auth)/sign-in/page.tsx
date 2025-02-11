d98424fe1e21d4252e83672b3fbc72338bae97eb
Aayush Gupta <AayushGuptap15@gmail.com>
2025-02-10 21:52:20 -0500

    Implement basic page for authform

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,9 +1,15 @@
+import AuthForm from '@/components/AuthForm'
 import React from 'react'
 
-const Signin = () => {
+const SignIn = () => {
   return (
-    <div>SignIn</div>
+    <section className="flex-center size-full max-sm:px-6">
+      <AuthForm type="sign-in"/>
+
+
+
+    </section>
   )
 }
 
-export default Signin
+export default SignIn
