6cea581b1758ebf3518e1dcb25d64d90048df399
Aayush Gupta <AayushGuptap15@gmail.com>
2025-02-13 16:03:05 -0500

    Implement signup page and switching basic

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -79,19 +79,32 @@ const AuthForm = ({type}: {type: string}) => {
             (<>
             <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                {type === 'sign-up' && (
+                  <>
+                    <CustomInput control={form.control} name='firstName' label='First Name' placeholder='Enter your first name'/>
+                    <CustomInput control={form.control} name='lastName' label='Last Name' placeholder='Enter your last name'/>
+                    <CustomInput control={form.control} name='address1' label='Address' placeholder='Enter your apecific address'/>
+                    <CustomInput control={form.control} name='state' label='State' placeholder='Example: NY'/>
+                    <CustomInput control={form.control} name='postalCode' label='Postal Code' placeholder='Example: 11101'/>
+                    <CustomInput control={form.control} name='dateOfBirth' label='Date of Birth' placeholder='YYYY-MM-DD'/>
+                    <CustomInput control={form.control} name='ssn' label='SSN' placeholder='Example: 1234'/>
+                  </>
+                )}
+
                 <CustomInput control={form.control} name='email' label='Email' placeholder='Enter your email'/>
 
                 <CustomInput control={form.control} name='password' label='Password' placeholder='Enter your password'/>
-                
-                <Button type="submit" disabled ={isLoading}className="form-btn"> 
-                    {isLoading ? (
-                        <>
-                            <Loader2 size={20}
-                            className="animate-spin" /> &nbsp; Loading...
-                        </>
-                    ): type === 'sign-in'
-                        ? 'Sign In' : 'Sign Up'}
-                </Button>
+                <div className='flex flex-col gap-4'>
+                    <Button type="submit" disabled ={isLoading}className="form-btn"> 
+                        {isLoading ? (
+                            <>
+                                <Loader2 size={20}
+                                className="animate-spin" /> &nbsp; Loading...
+                            </>
+                        ): type === 'sign-in'
+                            ? 'Sign In' : 'Sign Up'}
+                    </Button>
+                </div>
             </form>
             </Form>
 
@@ -102,7 +115,7 @@ const AuthForm = ({type}: {type: string}) => {
                     : "Already have an account?"}
                 </p>
                 <Link href={type === 'sign-in' ? '/sign-up' : '/sign-in'} className='form-link'>
-                    {type === 'sign-in' ? 'Sign up' : '/Sign in'}
+                    {type === 'sign-in' ? 'Sign up' : 'Sign in'}
                 </Link>
             </footer>
             </>
