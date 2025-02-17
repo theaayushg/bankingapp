64fb4107fe15b1bc3bb2c6fa47630699b15141d8
Aayush Gupta <AayushGuptap15@gmail.com>
2025-02-16 20:55:15 -0500

    Implementation of backend connection

--- lib/actions/user.actions.tsx
+++ lib/actions/user.actions.tsx
@@ -1,5 +1,10 @@
 'use server';
 
+import { ID } from "node-appwrite";
+import { createAdminClient, createSessionClient } from "../appwrite";
+import { cookies } from "next/headers";
+import { parseStringify } from "../utils";
+
 export const signIn = async () => {
     try {
         // mutation / databasee / make fetch
@@ -9,9 +14,38 @@ export const signIn = async () => {
 }
 
 export const signUp = async (userData: SignUpParams) => {
+    const { email, password, firstName, lastName } = userData;
+    
     try {
-        // mutation / databasee / make fetch
+        const { account } = await createAdminClient();
+
+        const newUserAccount = await account.create(
+            ID.unique(), 
+            email, 
+            password, 
+            `${firstName} ${lastName}` 
+        );
+
+        const session = await account.createEmailPasswordSession(email, password);
+      
+        cookies().set("appwrite-session", session.secret, {
+          path: "/",
+          httpOnly: true,
+          sameSite: "strict",
+          secure: true,
+        });
+
+        return parseStringify(newUserAccount);
     } catch (error) {
         console.error(error);
     }
 }
+
+export async function getLoggedInUser() {
+    try {
+      const { account } = await createSessionClient();
+      return await account.get();
+    } catch (error) {
+      return null;
+    }
+  }
